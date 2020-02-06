1f0a64c06c5958d970198194b5c2af209f40f1c4
Clément KOENIG <clement.koenig@unistra.fr>
2020-02-06 14:51:33 +0100

    Affiche le mode de jeu dans la liste des configs

--- serializers/config_serializer.js
+++ serializers/config_serializer.js
@@ -1,7 +1,7 @@
 const db = require('../models');
 
 const basicDetails = {
-    attributes: ['id', 'name', 'isPrivate', 'createdAt']
+    attributes: ['id', 'name', 'gameMode', 'isPrivate', 'createdAt']
 };
 
 const ownerUsername = {
