11204c0edac3bef29a6471d54869f03d1123bdc9
Clément KOENIG <clement.koenig@unistra.fr>
2020-02-09 10:13:06 +0100

    FIX documentation indentation

--- models/config.js
+++ models/config.js
@@ -4,41 +4,41 @@ module.exports = (sequelize, DataTypes) => {
     /**
      * @swagger
      *  components:
-     *      schemas:
-     *          Config:
-     *              type: object
-     *              required:
-     *                  - name
-     *                  - gameMode
-     *              properties:
-     *                  name:
-     *                      type: string
-     *                      description: Nom de la configuration.
-     *                  isPrivate:
-     *                      type: boolean
-     *                      default: true
-     *                      description: Si la configuration est visible uniquement par la personne qui l'a créée ou également par les autres utilisateurs.
-     *                  maxPlayers:
-     *                      type: integer
-     *                      minimum: 1
-     *                      maximum: 100
-     *                      description: Le nombre maximum de joueurs par équipe.
-     *                  gameMode:
-     *                      type: string
-     *                      in: query
-     *                      enum: ["FLAG", "TIME", "SUPREMACY"]
-     *                      description: Mode de jeu.
-     *                  duration:
-     *                      type: integer
-     *                      minimum: 60
-     *                      maximum: 31536000
-     *                      description: Durée de la partie en secondes, une partie doit durer au minimum 1 minute et au maximum 1 an. Le mode de jeu SUPREMACY ne peut pas avoir de limite de temps.
-     *                  inventorySize:
-     *                      type: integer
-     *                      minimum: 1
-     *                      maximum: 10
-     *                      default: 2
-     *                      description: Nombre d'objets qu'un joueur pourra avoir dans son inventaire.
+     *    schemas:
+     *      Config:
+     *        type: object
+     *        required:
+     *          - name
+     *          - gameMode
+     *        properties:
+     *          name:
+     *            type: string
+     *            description: Nom de la configuration.
+     *          isPrivate:
+     *            type: boolean
+     *            default: true
+     *            description: Si la configuration est visible uniquement par la personne qui l'a créée ou également par les autres utilisateurs.
+     *          maxPlayers:
+     *            type: integer
+     *            minimum: 1
+     *            maximum: 100
+     *            description: Le nombre maximum de joueurs par équipe.
+     *          gameMode:
+     *            type: string
+     *            in: query
+     *            enum: ["FLAG", "TIME", "SUPREMACY"]
+     *            description: Mode de jeu.
+     *          duration:
+     *            type: integer
+     *            minimum: 60
+     *            maximum: 31536000
+     *            description: Durée de la partie en secondes, une partie doit durer au minimum 1 minute et au maximum 1 an. Le mode de jeu SUPREMACY ne peut pas avoir de limite de temps.
+     *          inventorySize:
+     *            type: integer
+     *            minimum: 1
+     *            maximum: 10
+     *            default: 2
+     *            description: Nombre d'objets qu'un joueur pourra avoir dans son inventaire.
      */
     class Config extends Model {}
 
