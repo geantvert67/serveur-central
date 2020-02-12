217b1ab90df42c0f6c30eb4b4fd97d619f2afc98
clementkoenig <clement.koenig@unistra.fr>
2020-02-12 14:50:45 +0100

    Team GET and POST

---
+++ routes/team_routes.js
@@ -0,0 +1,87 @@
+const team_ctrl = require('../controllers/team_ctrl'),
+    config_ctrl = require('../controllers/config_ctrl');
+
+/**
+ * @swagger
+ * tags:
+ *   name: Team
+ */
+module.exports = [
+    {
+        url: '/configs/:config_id/teams',
+        method: 'use',
+        func: config_ctrl.isConfigOwner
+    },
+
+    /**
+     * @swagger
+     * path:
+     *   /configs/{config_id}/teams:
+     *     get:
+     *       summary: Lister toutes les équipes d'une configuration
+     *       tags: [Team]
+     *       security:
+     *         - JWTAuth : []
+     *       parameters:
+     *         - in: path
+     *           name: config_id
+     *           required: true
+     *           schema:
+     *             type: integer
+     *             minimum: 1
+     *           description: Identifiant de la configuration
+     *       responses:
+     *         "200":
+     *            description: Équipes récupérées
+     *            content:
+     *              application/json:
+     *                schema:
+     *                  type: array
+     *                  items: 
+     *                    $ref: '#/components/schemas/Team'
+     */
+    {
+        url: '/configs/:config_id/teams',
+        method: 'get',
+        func: team_ctrl.getAll
+    },
+
+    /**
+     * @swagger
+     * path:
+     *   /configs/{config_id}/teams:
+     *     post:
+     *       summary: Créer une équipe
+     *       tags: [Team]
+     *       security:
+     *         - JWTAuth : []
+     *       parameters:
+     *         - in: path
+     *           name: config_id
+     *           required: true
+     *           schema:
+     *             type: integer
+     *             minimum: 1
+     *           description: Identifiant de la configuration
+     *       requestBody:
+     *        required: true
+     *        content:
+     *          application/json:
+     *            schema:
+     *              $ref: '#/components/schemas/Team'
+     *       responses:
+     *         "200":
+     *           description: Équipe créée
+     *           content:
+     *             application/json:
+     *               schema:
+     *                 $ref: '#/components/schemas/Team'
+     *         "406":
+     *           description: Paramètres invalides
+     */
+    {
+        url: '/configs/:config_id/teams',
+        method: 'post',
+        func: team_ctrl.create
+    }
+]
