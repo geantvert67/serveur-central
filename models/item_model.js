30c8c66594b88d31ef232a1681ff21d181f87d7c
clementkoenig <clement.koenig@unistra.fr>
2020-02-18 09:33:14 +0100

    Item Model model

--- models/item_model.js
+++ models/item_model.js
@@ -1,13 +1,11 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class ItemModel extends Model {}
-
     /**
      * @swagger
      * components:
      *   schemas:
-     *     Item Model:
+     *     ItemModel:
      *       type: object
      *       required:
      *         - name
@@ -15,12 +13,50 @@ module.exports = (sequelize, DataTypes) => {
      *         - actionRadius
      *       properties:
      *         name:
-     *            type: string
-     *            minLength: 2
-     *            maxLength: 50
-     *            description: Nom de l'item.
+     *           type: string
+     *           minLength: 2
+     *           maxLength: 50
+     *           description: Nom de l'item.
+     *         visibilityRadius:
+     *           type: number
+     *           format: decimal
+     *           minimum: 0.01
+     *           description: Rayon de visibilité de l'item.
+     *         actionRadius:
+     *           type: number
+     *           format: decimal
+     *           minimum: 0.01
+     *           description: Rayon d'action de l'item. Doit être inférieur ou égal au rayon de visibilité.
+     *         waitingPeriode:
+     *           type: integer
+     *           minimum: 1
+     *           maximum: 31536000
+     *           description: Période de carence (en secondes) durant laquelle un item est visible mais ne peut pas être ramassé.
+     *         autoMove:
+     *           type: boolean
+     *           default: false
+     *           description: Si l'item va se téléporter aléatoirement à chaque fois qu'un joueur en ramasse.
+     *         delay: 
+     *           type: integer
+     *           minimum: 1
+     *           maximum: 31536000
+     *           description: Délai d'activation de l'item en secondes.
      *       example:
+     *         name: Mine
+     *         visibilityRadius: 1.5
+     *         actionRadius: 1.25
+     *         waitingPeriode: 600
+     *         autoMove: false
+     *         delay: 60
      */
+    class ItemModel extends Model {
+        static nameIsUnique(name, ConfigId) {
+            return this.findOne({ where: { name, ConfigId } })
+                .then(i => (i ? false : true))
+                .catch(err => false);
+        }
+    }
+
     ItemModel.init(
         {
             name: {
@@ -73,8 +109,14 @@ module.exports = (sequelize, DataTypes) => {
         },
         {
             sequelize,
-            modelName: 'Item Model',
-            timestamps: false
+            modelName: 'ItemModel',
+            timestamps: false,
+            indexes: [
+                {
+                    unique: true,
+                    fields: ['ConfigId', 'name']
+                }
+            ]
         }
     );
 
