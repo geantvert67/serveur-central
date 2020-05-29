e4d90660e817f2f185a70e3c687b3a6781f1c64e
Clément KOENIG <clement.koenig@unistra.fr>
2020-05-29 18:03:04 +0200

    Documentation => history

--- models/history.js
+++ models/history.js
@@ -1,6 +1,61 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
+    /**
+     * @swagger
+     * components:
+     *   schemas:
+     *     History:
+     *       type: object
+     *       required:
+     *         - score
+     *         - nbFlags
+     *         - nbDiscoveredFlags
+     *         - nbTraps
+     *         - hasWon
+     *         - hasLost
+     *         - teamName
+     *         - teamColor
+     *         - teamScore
+     *       properties:
+     *         score:
+     *           type: integer
+     *           description: Score du joueur
+     *         nbFlags:
+     *           type: integer
+     *           description: Nombre de drapeaux capturés par le joueur
+     *         nbDiscoveredFlags:
+     *           type: integer
+     *           description: Nombre de drapeaux découverts par le joueur
+     *         nbTraps:
+     *           type: integer
+     *           description: Nombre de pièges du joueur qui ont fait mouche
+     *         hasWon:
+     *           type: boolean
+     *           description: Si l'équipe du joueur a gagné ou non
+     *         hasLost:
+     *           type: boolean
+     *           description: Si l'équipe du joueur a perdu ou non
+     *         teamName:
+     *           type: string
+     *           description: Nom de l'équipe du joueur
+     *         teamColor:
+     *           type: string
+     *           description: Couleur de l'équipe du joueur
+     *         teamScore:
+     *           type: string
+     *           description: Score de l'équipe du joueur
+     *       example:
+     *         score: 5
+     *         nbFlags: 5
+     *         nbDiscoveredFlags: 1
+     *         nbTraps: 0
+     *         hasWon: false
+     *         hasLost: true
+     *         teamName: équipe 1
+     *         teamColor: #e6e6e6
+     *         teamScore: 36
+     */
     class History extends Model {}
 
     History.init(
